3469b6f9f3b08377e631db20964a3e8dea9e6175
Попов Сергей <s-s1990@yandex.ru>
2018-11-10 05:40:33 +0300

    Updates

---
+++ precache-manifest.474111d1bdcdb0c49d359195d6e33a01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "90b1980cd6d17089cc0f",
+    "url": "/ray/static/js/runtime~main.90b1980c.js"
+  },
+  {
+    "revision": "075c9d32c9bad52de7c3",
+    "url": "/ray/static/js/main.075c9d32.chunk.js"
+  },
+  {
+    "revision": "cc5a22bdd12626d457ef",
+    "url": "/ray/static/js/1.cc5a22bd.chunk.js"
+  },
+  {
+    "revision": "cc5a22bdd12626d457ef",
+    "url": "/ray/static/css/1.bb0c19c8.chunk.css"
+  },
+  {
+    "revision": "01db74f8f2b9db201de1a6645b03c82c",
+    "url": "/ray/index.html"
+  }
+];
